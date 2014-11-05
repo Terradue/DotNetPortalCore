cffce11bf8e21580189edfcc017955055caf7858
eboissier <eboissier@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-11-05 15:14:50 +0000

    Merge branch 'hotfix/11887'
    git-svn-id: https://gateway.terradue.com/repo/products/sugar/Terradue.Portal/trunk@39258 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

---
+++ Terradue.Portal.Test/WpsProcessOffering.Test.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
+using Mono.Addins;
+using System.Collections.Specialized;
+using Terradue.Portal;
+using Terradue.ServiceModel.Ogc.OwsContext;
+
+namespace Terradue.Portal.Test {
+
+    [TestFixture]
+    public class WpsProcessOfferingTest : BaseTest{
+    
+        [TestFixtureSetUp]
+        public void FixtureSetup(){
+            base.FixtureSetup();
+            context.BaseUrl = "http://localhost:8080/api";
+        }
+
+        private WpsProcessOffering CreateProcess(bool proxy){
+            WpsProvider provider;
+            provider = new WpsProvider(context);
+            provider.Name = "test provider" + (proxy ? "proxy" : "no proxy");
+            provider.BaseUrl = "http://dem.terradue.int:8080/wps/WebProcessingService";
+            provider.Proxy = proxy;
+            try{
+                provider.Store();
+            }catch(Exception e){
+                throw e;
+            }
+
+            WpsProcessOffering process = new WpsProcessOffering(context);
+            process.RemoteIdentifier = "com.test.provider";
+            process.Identifier = Guid.NewGuid().ToString();
+            process.Url = "http://dem.terradue.int:8080/wps/WebProcessingService";
+            process.Version = "1.0.0";
+            process.Provider = provider;
+
+            return process;
+        }
+
+        [Test]
+        public void GetAtomItemWithProxy() {
+            WpsProcessOffering process = CreateProcess(true);
+            var entry = process.ToAtomItem(new NameValueCollection());
+            OwsContextAtomEntry result = new OwsContextAtomEntry(entry);
+            Assert.That(result.Offering != null);
+        }
+
+        [Test]
+        public void GetAtomItemWithoutProxy() {
+            WpsProcessOffering process = CreateProcess(false);
+            var entry = process.ToAtomItem(new NameValueCollection());
+            OwsContextAtomEntry result = new OwsContextAtomEntry(entry);
+            Assert.That(result.Offering != null);
+        }
+
+    }
+}
+
