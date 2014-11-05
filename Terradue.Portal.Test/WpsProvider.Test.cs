9deb4e69a90569167eb19755306c2d1ef5af2c7e
eboissier <eboissier@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-11-05 15:58:39 +0000

    Merge branch 'hotfix/2.5.9patch'
    git-svn-id: https://gateway.terradue.com/repo/products/sugar/Terradue.Portal/trunk@39264 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

--- Terradue.Portal.Test/WpsProvider.Test.cs
+++ Terradue.Portal.Test/WpsProvider.Test.cs
@@ -6,27 +6,31 @@ using OpenGis.Wps;
 namespace Terradue.Portal.Test {
 
     [TestFixture()]
-    public class WpsProviderTest {
+    public class WpsProviderTest : BaseTest{
 
         WpsProvider provider;
-        WPSCapabilitiesType capabilities;
 
-        [SetUp]
-        public void Setup(){
-            IfyWebContext context = new IfyWebContext(PagePrivileges.EverybodyView);
+        [TestFixtureSetUp]
+        public void FixtureSetup(){
+            base.FixtureSetup();
+            context.BaseUrl = "http://localhost:8080/api";
+
             provider = new WpsProvider(context);
             provider.Name = "test-wpsprovider";
-            provider.BaseUrl = "http://dem.terradue.int:8080/wps/WebProcessingService?service=wps&request=getCapabilities";
+            provider.BaseUrl = "http://dem.terradue.int:8080/wps/WebProcessingService";
+            provider.Proxy = false;
         }
 
         [Test()]
         public void GetCapabilities() {
-            capabilities = WpsProvider.GetWPSCapabilitiesFromUrl(provider.BaseUrl);
+            WPSCapabilitiesType capabilities = WpsProvider.GetWPSCapabilitiesFromUrl(provider.BaseUrl);
             Assert.That(capabilities.ProcessOfferings.Process.Count > 0);
         }
 
         [Test()]
-        public void GetDescribeProcess() {
+        public void StoreProcessOffering() {
+            provider.Store();
+            provider.StoreProcessOfferings();
             List<ProcessBriefType> process = provider.GetProcessOfferings();
             Assert.That(process.Count == 1);
             Assert.That(process[0].Title != null);
