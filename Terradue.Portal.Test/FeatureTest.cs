69fe8a1d542ac218215175323b1abd9efa47e33c
eboissier <enguerran.boissier@terradue.com>
2015-07-28 16:34:06 +0200

    update feature store (position)

--- Terradue.Portal.Test/FeatureTest.cs
+++ Terradue.Portal.Test/FeatureTest.cs
@@ -8,21 +8,18 @@ namespace Terradue.Portal.Test {
 
         [Test]
         public void FeatureCreationTest(){
-
-            Feature feat3 = new Feature(context);
-            feat3.Title = "feat3";
-            feat3.Position = 3;
-            feat3.Store();
-
+            
             Feature feat1 = new Feature(context);
             feat1.Title = "feat1";
-            feat1.Position = 1;
             feat1.Store();
 
             Feature feat2 = new Feature(context);
             feat2.Title = "feat2";
-            feat2.Position = 2;
             feat2.Store();
+
+            Feature feat3 = new Feature(context);
+            feat3.Title = "feat3";
+            feat3.Store();
         }
 
         [Test]
@@ -50,6 +47,21 @@ namespace Terradue.Portal.Test {
             Assert.That(fs[0].Title.Equals("feat3"));
             Assert.That(fs[1].Title.Equals("feat2"));
             Assert.That(fs[2].Title.Equals("feat1"));
+
+            //insert new feature in the middle
+            Feature feat4 = new Feature(context);
+            feat4.Title = "feat4";
+            feat4.Position = 2;
+            feat4.Store();
+
+            features.Load();
+            fs = features.GetItemsAsList();
+            fs.Sort();
+
+            Assert.That(fs[0].Title.Equals("feat3"));
+            Assert.That(fs[1].Title.Equals("feat4"));
+            Assert.That(fs[2].Title.Equals("feat2"));
+            Assert.That(fs[3].Title.Equals("feat1"));
         }
     }
 }
