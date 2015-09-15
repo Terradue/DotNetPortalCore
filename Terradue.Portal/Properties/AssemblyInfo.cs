a21feb8a175612392b6471bd2492d13b3498d7d8
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-09-15 11:51:16 +0200

    ready to pack 2.6.41
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.Portal/Properties/AssemblyInfo.cs
+++ Terradue.Portal/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.CompilerServices;
 @{
     Terradue.Portal Software Package
 
-    \xrefitem sw_version "Versions" "Software Package Version" 2.6.40
+    \xrefitem sw_version "Versions" "Software Package Version" 2.6.41
 
     \xrefitem sw_link "Links" "Software Package List" [Terradue.Portal](https://git.terradue.com/sugar/terradue-portal)
 
@@ -35,8 +35,8 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("2.6.40.*")]
-[assembly: AssemblyInformationalVersion ("2.6.40")]
+[assembly: AssemblyVersion ("2.6.41.*")]
+[assembly: AssemblyInformationalVersion ("2.6.41")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
