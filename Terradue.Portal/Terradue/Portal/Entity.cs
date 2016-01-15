529eb7c5f98ed177d0f06e6d7d6852e2d4dd6988
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-01-15 12:34:47 +0100

    doc
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.Portal/Terradue/Portal/Entity.cs
+++ Terradue.Portal/Terradue/Portal/Entity.cs
@@ -37,7 +37,7 @@ It also implements the low level functions to store and read data persistently o
 @{
 
 The Security component is a set of library in charge with all the authorisation or authentication functions and also with the privileges management 
-between users, groups and other business objects.The security scheme is open and offers many possibilities to plug other component to
+between users, groups and other business objects. The security scheme is open and offers many possibilities to plug other component to
 implement specific authentication mechanism or authorization scheme.
 
 @}
