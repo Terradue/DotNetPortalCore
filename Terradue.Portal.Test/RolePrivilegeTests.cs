6c545a489646fd5aef8488383aea40f34a140753
frank.loeschau@terradue.com <floeschau@users.noreply.github.com>
2016-03-17 15:34:41 +0000

    New tests for role privileges

--- Terradue.Portal.Test/RolePrivilegeTests.cs
+++ Terradue.Portal.Test/RolePrivilegeTests.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using NUnit.Framework;
 
 namespace Terradue.Portal.Test {
+
+    [TestFixture]
     public class RolePrivilegeTests {
-        public RolePrivilegeTests() {
+
+        [TestFixtureSetUp]
+        public void CreateEnvironment() {
+
+/*            string connectionString = "Server=localhost; Port=3306; User Id=root; Database=TerraduePortalTest";
+            AdminTool adminTool = new AdminTool();
+            adminTool.Process();*/
+
         }
+
+
+        [Test]
+        public void Test() {
+            Assert.That(true);
+        }
+
+        [TestFixtureTearDown]
+        public void DestroyEnvironment() {
+        }
+
     }
+
 }
 
