731d5e3e9e97031fe1fb1b219c3ee2ece21f8574
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-03-23 15:04:03 +0100

    more basic privileges
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.Portal/Terradue/Portal/Entity.cs
+++ Terradue.Portal/Terradue/Portal/Entity.cs
@@ -86,7 +86,7 @@ The following defintions supports this scheme:
 - A \ref Role defines the set of privileges that are granted to a \ref User or a \ref Group for a specific \ref Domain or globally.
 - The assignement of a \ref Role to a \ref User or a \ref Group is called a "Role Grant". A Role Grant can be associated to a \ref Domain and is therefore called "Domain Role Grant".
 A Role Grant not associated to any domain is a "Global Role Grant".
-- A Privilege is an access control for a given entity (object) type. For all objects, there are 3 basic privileges : "Can Create", "Cen Change","Can Delete".
+- A Privilege is an access control for a given entity (object) type. For all objects, there are 6 basic privileges : "Can Create", "Can Change", "Can Delete", "Can View", "Can Search" and "Can Manage".
 For instance, "Can Create" for \ref Series specify the possibility to create a \Series in the system
 - A Permission is a specific Privilege for a \ref User or a \ref Group for a given Object (\ref Entity). For instance: "Can View" for the ENVISAT \ref Series speificy the possibility to view the ENVISAT \ref Series in the results of a search.
 
