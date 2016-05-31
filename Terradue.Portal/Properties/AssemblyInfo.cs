77403da163ba0c8cf2e889766555bf6a31284f15
eboissier <enguerran.boissier@terradue.com>
2016-05-31 15:06:02 +0200

    up assembly

--- Terradue.Portal/Properties/AssemblyInfo.cs
+++ Terradue.Portal/Properties/AssemblyInfo.cs
@@ -1,6 +1,3 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
 /*!
 
 \namespace Terradue.Portal
@@ -22,23 +19,22 @@ using System.Runtime.CompilerServices;
 
 */
 
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
-[assembly: AssemblyTitle ("Terradue.Portal")]
-[assembly: AssemblyDescription ("Terradue .Net Portal Library")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("Terradue")]
-[assembly: AssemblyProduct ("Terradue.Portal")]
-[assembly: AssemblyCopyright ("Terradue")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("2.6.43.*")]
-[assembly: AssemblyInformationalVersion ("2.6.43")]
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using NuGet4Mono.Extensions;
 
+[assembly: AssemblyTitle("Terradue.Portal")]
+[assembly: AssemblyDescription("Terradue.Portal is a library targeting .NET 4.0 and above that provides core interfaces and classes of Terradue portal")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Terradue")]
+[assembly: AssemblyProduct("Terradue.Portal")]
+[assembly: AssemblyCopyright("Terradue")]
+[assembly: AssemblyAuthors("Enguerran Boissier")]
+[assembly: AssemblyProjectUrl("https://git.terradue.com/sugar/terradue-portal")]
+[assembly: AssemblyLicenseUrl("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("2.6.43")]
+[assembly: AssemblyInformationalVersion("2.6.43")]
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
