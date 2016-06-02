e4f5bbf7e8a609f7c265289f375952b1c43b0009
frank.loeschau@terradue.com <floeschau@users.noreply.github.com>
2016-06-02 16:57:24 +0100

    Added unit test for EntityCollection filtering

--- Terradue.Portal.Test/EntityCollectionTest.cs
+++ Terradue.Portal.Test/EntityCollectionTest.cs
@@ -21,12 +21,12 @@ namespace Terradue.Portal.Test {
 
             context.StartImpersonation(user1.Id);
             PublishServer p1 = new PublishServer(context);
-            p1.Identifier = "p1";
+            p1.Name = "p1";
             p1.Protocol = "ftp";
             p1.Hostname = "test.org";
             p1.Store();
             PublishServer p2 = new PublishServer(context);
-            p2.Identifier = "p2";
+            p2.Name = "p2";
             p2.Protocol = "ftp";
             p2.Hostname = "test.org";
             p2.Store();
@@ -34,7 +34,7 @@ namespace Terradue.Portal.Test {
 
             context.StartImpersonation(user2.Id);
             PublishServer p3 = new PublishServer(context);
-            p3.Identifier = "p3";
+            p3.Name = "p3";
             p3.Protocol = "ftp";
             p3.Hostname = "test.org";
             p3.Store();
@@ -58,6 +58,65 @@ namespace Terradue.Portal.Test {
 
             context.EndImpersonation();
         }
+
+
+        [Test]
+        public void FilterTest() {
+            context.AccessLevel = EntityAccessLevel.Administrator;
+
+            PublishServer p1 = new PublishServer(context);
+            p1.Name = "pf1";
+            p1.Protocol = "ftp";
+            p1.Hostname = "test.org";
+            p1.Port = 23;
+            p1.Store();
+            PublishServer p2 = new PublishServer(context);
+            p2.Name = "pf2";
+            p2.Protocol = "ftp";
+            p2.Hostname = "anothertest.org";
+            p2.Port = 123;
+            p2.Store();
+            PublishServer p3 = new PublishServer(context);
+            p3.Name = "pf3";
+            p3.Protocol = "ftp";
+            p3.Hostname = "experiment.org";
+            p3.Port = 234;
+            p3.Store();
+            PublishServer p4 = new PublishServer(context);
+            p4.Name = "pf4";
+            p4.Protocol = "ftp";
+            p4.Hostname = "try.org";
+            p4.Port = 345;
+            p4.Store();
+            context.EndImpersonation();
+
+            EntityDictionary<PublishServer> pd1 = new EntityDictionary<PublishServer>(context);
+            pd1.SetFilter("Hostname", "*test*.org");
+            pd1.Load();
+            Assert.IsTrue(pd1.Count == 2);
+            Assert.IsTrue(pd1.Contains(p1.Id) && pd1.Contains(p2.Id));
+
+            EntityDictionary<PublishServer> pd2 = new EntityDictionary<PublishServer>(context);
+            pd2.SetFilter("Port", "123");
+            pd2.Load();
+            Assert.IsTrue(pd2.Count == 1);
+            Assert.IsTrue(pd2.Contains(p2.Id));
+
+            context.ConsoleDebug = true;
+            EntityDictionary<PublishServer> pd3 = new EntityDictionary<PublishServer>(context);
+            pd3.SetFilter("Port", "[234");
+            pd3.Load();
+            Assert.IsTrue(pd3.Count == 2);
+            Assert.IsTrue(pd3.Contains(p3.Id) && pd3.Contains(p4.Id));
+
+            EntityDictionary<PublishServer> pd4 = new EntityDictionary<PublishServer>(context);
+            pd4.SetFilter("Port", "]100,300[");
+            pd4.Load();
+            Assert.IsTrue(pd4.Count == 2);
+            Assert.IsTrue(pd4.Contains(p2.Id) && pd4.Contains(p3.Id));
+
+        }
     }
+
 }
 
