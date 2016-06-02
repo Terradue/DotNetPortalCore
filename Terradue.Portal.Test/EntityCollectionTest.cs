eb40466d89bc3879694134cad43683c8e029ac00
frank.loeschau@terradue.com <floeschau@users.noreply.github.com>
2016-06-02 15:18:43 +0100

    EntityCollection unit test (draft)

--- Terradue.Portal.Test/EntityCollectionTest.cs
+++ Terradue.Portal.Test/EntityCollectionTest.cs
@@ -1,8 +1,62 @@
 ﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Terradue.Portal.Test {
-    public class EntityCollection {
-        public EntityCollection() {
+
+    [TestFixture]
+    public class EntityCollectionTest : BaseTest {
+
+        [Test]
+        public void LoadingTest() {
+            context.AccessLevel = EntityAccessLevel.Administrator;
+
+            User user1 = new User(context);
+            user1.Username = "user1";
+            user1.Store();
+
+            User user2 = new User(context);
+            user2.Username = "user2";
+            user2.Store();
+
+            context.StartImpersonation(user1.Id);
+            PublishServer p1 = new PublishServer(context);
+            p1.Identifier = "p1";
+            p1.Protocol = "ftp";
+            p1.Hostname = "test.org";
+            p1.Store();
+            PublishServer p2 = new PublishServer(context);
+            p2.Identifier = "p2";
+            p2.Protocol = "ftp";
+            p2.Hostname = "test.org";
+            p2.Store();
+            context.EndImpersonation();
+
+            context.StartImpersonation(user2.Id);
+            PublishServer p3 = new PublishServer(context);
+            p3.Identifier = "p3";
+            p3.Protocol = "ftp";
+            p3.Hostname = "test.org";
+            p3.Store();
+            context.EndImpersonation();
+
+            context.StartImpersonation(user1.Id);
+            EntityDictionary<PublishServer> pd1 = new EntityDictionary<PublishServer>(context);
+            pd1.OwnedItemsOnly = true;
+            pd1.Load();
+            Assert.IsTrue(pd1.Count == 2);
+            context.EndImpersonation();
+
+            context.StartImpersonation(user2.Id);
+            EntityDictionary<PublishServer> pd2 = new EntityDictionary<PublishServer>(context);
+            Assert.Throws<EntityNotFoundException>(delegate { 
+                pd2.LoadFromSource(pd1, false);
+            });
+
+            pd2.LoadFromSource(pd1, true);
+            Assert.IsTrue(pd2.Count == 2);
+
+            context.EndImpersonation();
         }
     }
 }
