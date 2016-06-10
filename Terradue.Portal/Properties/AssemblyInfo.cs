0ba356c020827d407a87961536a9db75fe9762cd
eboissier <enguerran.boissier@terradue.com>
2016-06-10 09:46:45 +0200

    up version

--- Terradue.Portal/Properties/AssemblyInfo.cs
+++ Terradue.Portal/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using NuGet4Mono.Extensions;
 [assembly: AssemblyLicenseUrl("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.6.56")]
-[assembly: AssemblyInformationalVersion("2.6.56")]
+[assembly: AssemblyVersion("2.6.57")]
+[assembly: AssemblyInformationalVersion("2.6.57")]
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
