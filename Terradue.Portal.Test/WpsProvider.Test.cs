d2ff2ade92ebd70ac73823981bcbcd4fc3057c39
eboissier <enguerran.boissier@terradue.com>
2016-10-07 14:03:10 +0200

    add test

--- Terradue.Portal.Test/WpsProvider.Test.cs
+++ Terradue.Portal.Test/WpsProvider.Test.cs
@@ -5,10 +5,12 @@ using OpenGis.Wps;
 
 namespace Terradue.Portal.Test {
 
-    [TestFixture()]
+    [TestFixture]
     public class WpsProviderTest : BaseTest{
 
         WpsProvider provider;
+        WpsProvider providerSecret;
+        string processIdentifier, processVersion, processSecretIdentifier, processSecretVersion;
 
         [TestFixtureSetUp]
         public void FixtureSetup(){
@@ -20,15 +22,48 @@ namespace Terradue.Portal.Test {
             provider.Name = "test-wpsprovider";
             provider.BaseUrl = "http://dem.terradue.int:8080/wps/WebProcessingService";
             provider.Proxy = false;
+
+            providerSecret = new WpsProvider (context);
+            providerSecret.Identifier = "wpsproviderSecret";
+            providerSecret.Name = "test-wpsproviderSecret";
+            providerSecret.BaseUrl = "http://urban1:qeF2pzNm@www.brockmann-consult.de/bc-wps/wps/calvalus";
+            providerSecret.Proxy = true;
+
+            processIdentifier = "com.terradue.wps_oozie.process.OozieAbstractAlgorithm";
+            processVersion = "1.0.0";
+
+            processSecretIdentifier = "urbantep-local~1.0~Subset";
+            processSecretVersion = "1.0";
         }
 
-        [Test()]
+        [Test]
         public void GetCapabilities() {
-            WPSCapabilitiesType capabilities = WpsProvider.GetWPSCapabilitiesFromUrl(provider.BaseUrl);
+            var capabilities = provider.GetWPSCapabilities();
             Assert.That(capabilities.ProcessOfferings.Process.Count > 0);
         }
 
-        [Test()]
+        [Test]
+        public void DescribeProcess ()
+        {
+            var describe = provider.GetWPSDescribeProcess (processIdentifier, processVersion);
+            Assert.That (describe.DataInputs.Count > 0 );
+        }
+
+        [Test]
+        public void GetCapabilitiesSecret ()
+        {
+            var capabilities = providerSecret.GetWPSCapabilities ();
+            Assert.That (capabilities.ProcessOfferings.Process.Count > 0);
+        }
+
+        [Test]
+        public void DescribeProcessSecret ()
+        {
+            var describe = providerSecret.GetWPSDescribeProcess (processSecretIdentifier, processSecretVersion);
+            Assert.That (describe.DataInputs.Count > 0);
+        }
+
+        [Test]
         public void StoreProcessOffering() {
             provider.Store();
             provider.StoreProcessOfferings();
