e50f1f2972473883e9020c7b7f889bcbfda9a602
eboissier <enguerran.boissier@terradue.com>
2016-10-20 14:32:10 +0200

    add cookie

--- core/db/db-2.6.66#.sql
+++ core/db/db-2.6.66#.sql
@@ -4,4 +4,14 @@ USE $MAIN$;
 ALTER TABLE resourceset ADD COLUMN `id_type` INT(10) UNSIGNED NULL DEFAULT NULL;
 -- RESULT
 
+CREATE TABLE cookie (
+	session VARCHAR(100) NOT NULL COMMENT 'Session',
+    identifier VARCHAR(100) NOT NULL COMMENT 'Identifier',
+    value TEXT NULL COMMENT 'Value',
+	expire datetime,    
+	creation_date datetime,
+	UNIQUE INDEX (session,identifier)
+) Engine=InnoDB COMMENT 'DB Cookies';
+-- RESULT 
+
 /*****************************************************************************/
