59fdda996bd4c2e9028dd8760b5c6859724419ef
eboissier <enguerran.boissier@terradue.com>
2017-02-24 14:51:21 +0100

    up db index domain

--- core/db/db-2.7.4#.sql
+++ core/db/db-2.7.4#.sql
@@ -7,7 +7,7 @@ ALTER TABLE domain
 CHANGE COLUMN name name VARCHAR(200) NULL COMMENT 'Unique name' ,
 ADD COLUMN identifier VARCHAR(100) NOT NULL AFTER `id`,
 ADD UNIQUE INDEX identifier_UNIQUE (`identifier` ASC),
-DROP INDEX name ;
+DROP INDEX name_UNIQUE;
 
 UPDATE domain SET identifier=name;
 -- RESULT
