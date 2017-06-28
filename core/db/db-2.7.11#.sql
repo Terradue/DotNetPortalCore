faca1c8e6c6bf620981ea30923c3913b3b4c0537
eboissier <enguerran.boissier@terradue.com>
2017-06-28 15:26:54 +0200

    db update

---
+++ core/db/db-2.7.11#.sql
@@ -0,0 +1,9 @@
+USE $MAIN$;
+
+/*****************************************************************************/
+
+-- Update usr for unique email ... \
+ALTER TABLE usr ADD UNIQUE INDEX `email_UNIQUE` (`email` ASC);
+-- RESULT
+
+
