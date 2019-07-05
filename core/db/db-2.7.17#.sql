b510dbd791c413bd4bb4d6e8bbdfc894d7e9ce09
eboissier <enguerran.boissier@terradue.com>
2019-07-05 14:11:09 +0200

    up db

--- core/db/db-2.7.17#.sql
+++ core/db/db-2.7.17#.sql
@@ -11,3 +11,32 @@ CREATE TABLE wpsprovider_dev (
     CONSTRAINT fk_wpsprovider_dev_usr FOREIGN KEY (id_usr) REFERENCES usr(id) ON DELETE CASCADE
 ) Engine=InnoDB COMMENT 'User developers of WPS providers';
 -- RESULT 
+
+/*****************************************************************************/
+
+-- Create table gitrepo ... \
+CREATE TABLE gitrepo (
+    id int unsigned NOT NULL auto_increment,
+    url VARCHAR(300) NOT NULL COMMENT 'Git Repository URL',
+    id_usr int unsigned COMMENT 'User creating the repo',
+    id_domain int unsigned COMMENT 'Domain of the repo',
+    kind int unsigned COMMENT 'Git repo kind (gitlab, github, ...)',
+    CONSTRAINT pk_gitrepo PRIMARY KEY (id)
+) Engine=InnoDB COMMENT 'GIT repository';
+-- RESULT 
+
+/*****************************************************************************/
+
+-- Create table gitrepo_perm ... \
+CREATE TABLE gitrepo_perm (
+    id_gitrepo int unsigned NOT NULL COMMENT 'FK: Git repository',
+    id_usr int unsigned COMMENT 'FK: User',
+    id_grp int unsigned COMMENT 'FK: Group',
+    CONSTRAINT fk_gitrepo_perm_gitrepo FOREIGN KEY (id_gitrepo) REFERENCES gitrepo(id) ON DELETE CASCADE,
+    CONSTRAINT fk_gitrepo_perm_usr FOREIGN KEY (id_usr) REFERENCES usr(id) ON DELETE CASCADE,
+    CONSTRAINT fk_gitrepo_perm_grp FOREIGN KEY (id_grp) REFERENCES grp(id) ON DELETE CASCADE
+) Engine=InnoDB COMMENT 'User/group permissions on GIT repository';
+-- RESULT 
+
+/*****************************************************************************/
+
