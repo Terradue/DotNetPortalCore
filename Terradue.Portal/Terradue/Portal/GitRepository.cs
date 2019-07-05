beb377c72566052ce4882a96007e8a788db8612e
eboissier <enguerran.boissier@terradue.com>
2019-07-05 14:11:41 +0200

    Gitrepository - add kind + store check url exists

--- Terradue.Portal/Terradue/Portal/GitRepository.cs
+++ Terradue.Portal/Terradue/Portal/GitRepository.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Net;
+
 namespace Terradue.Portal {
     [EntityTable("gitrepo", EntityTableConfiguration.Custom, HasPermissionManagement = true, HasOwnerReference = true, HasDomainReference = true)]
     public class GitRepository : Entity {
+
+        [EntityDataField("kind")]
+        public int Kind { get; set; }
 
         [EntityDataField("url")]
         public string Url { get; set; }
@@ -14,5 +19,35 @@ namespace Terradue.Portal {
             repo.Load();
             return repo;
         }
+
+        public override void Store() {
+            //check url is not null
+            if (string.IsNullOrEmpty(this.Url)) throw new Exception("Invalid Git repository url");
+
+            //check url finishes with .git
+            try {
+                var urib = new UriBuilder(this.Url);
+                if (!urib.Path.EndsWith(".git", StringComparison.CurrentCulture)) throw new Exception("Invalid Git repository url");                
+            } catch (Exception e) {
+                throw new Exception("Invalid Git repository url");
+            }
+
+            //check url exists
+            var request = (HttpWebRequest)WebRequest.Create(this.Url);
+            request.Method = "HEAD";
+            try {
+                using (var httpResponse = (HttpWebResponse)request.GetResponse()) { }
+            } catch (Exception e) {
+                throw new Exception("Invalid Git repository url");
+            }
+
+            base.Store();
+        }
     }
+
+    public class GitRepositoryKind {
+        public static int GITLAB = 1;
+        public static int GITHUB = 2;
+    }
+
 }
