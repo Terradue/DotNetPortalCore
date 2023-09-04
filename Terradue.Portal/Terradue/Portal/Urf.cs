07afb6f74916df07ae25419d9f103afb062aff55
eboissier <enguerran.boissier@terradue.com>
2023-09-04 15:09:49 +0200

    ASD, add negative credit

--- Terradue.Portal/Terradue/Portal/Urf.cs
+++ Terradue.Portal/Terradue/Portal/Urf.cs
@@ -65,6 +65,8 @@ namespace Terradue.Portal.Urf {
         [DataMember]
         public double Credit { get; set; }
         [DataMember]
+        public bool NegativeCredit { get; set; }
+        [DataMember]
         public string PurchaseAction { get; set; }
         [DataMember]
         public string ServiceType { get; set; }
